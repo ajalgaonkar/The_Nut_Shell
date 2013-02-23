8c3d7ea938b71a0635d6368f509282e8809db7c8
varun-theone <varun.theone@gmail.com>
2013-02-22 22:17:22 -0500

    added support to non built in commands

--- base/support.c
+++ base/support.c
@@ -14,6 +14,7 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <pwd.h>
+
 /*
  *
  *  FatalError(sb):  print message and exit.
@@ -52,26 +53,31 @@ char *sb;
 /* USER ajalgaonkar DEFINED SECTION */
 void identify_word(char *wrd)
 {
+	char *args[] = {"command", (char *) 0 };  //Empty args
+	
     printf("Enter in word identify!!  %s\n",wrd);
     if(wrd==NULL)
     printf("Error: No word entered\n");
     if(strcmp(wrd,"dir") == 0)
         get_dir(wrd);
-    if(strcmp(wrd,"bye") == 0)
+    else if(strcmp(wrd,"bye") == 0)
         exit_flag = 1;
-    if(strcmp(wrd,"cd")==0)
+    else if(strcmp(wrd,"cd")==0)
     {
         struct passwd *pw = getpwuid(getuid());
         const char *homedir = pw->pw_dir;
         if(chdir(homedir) == 0)
             printf("Now on '%s'\n",homedir);
     }
-    if(strncmp(wrd,"~",1) == 0)
+    else if(strncmp(wrd,"~",1) == 0)
     {
         printf("Enter Expansion");
         char *expansion = tilde_expansion(wrd);
         printf("Directory is: %s\n",expansion);
     }
+	
+	else if(findAndExecute(wrd,args,NULL,NULL))
+		printf("%s: command not found \n",wrd);
 }
 
 
@@ -205,3 +211,142 @@ void get_dir(char *wrd)
             printf("\n");
         }
 }
+
+
+
+//// Varun functions
+
+//Returns  0 if file found and is executable
+//Returns -1 if file is not found 
+//Returns -2 if file is but not executable
+int checkIfExecutable(char *filePath)
+{
+	struct stat sb;
+	if (stat(filePath, &sb) == 0) //File present
+	{
+		if(S_ISREG(sb.st_mode) && sb.st_mode & 0111)
+		{
+			printf("%s is executable\n", filePath);
+			return 0;
+		}
+		return -2;
+	}
+	return -1;
+}
+
+
+//Returns  0 if Dir is found
+//Returns -1 if path is not found
+//Returns -2 if path is found but not a dir
+int checkIfDir(char *dirPath)
+{
+	struct stat sb;
+	if (stat(dirPath, &sb) == 0) //Dir present
+	{
+		if(S_ISDIR(sb.st_mode))
+		{
+			//printf("%s is a directory\n", dirPath);
+			return 0;
+		}
+		return -2;
+	}
+	return -1;
+}
+
+
+int set_read(int* rpipe)
+{
+    dup2(rpipe[0], STDIN_FILENO);
+    close(rpipe[0]); // we have a copy already, so close it
+    close(rpipe[1]); // not using this end
+	return 0;
+}
+  
+int set_write(int* wpipe)
+{
+    dup2(wpipe[1], STDOUT_FILENO);
+    close(wpipe[0]); // not using this end
+    close(wpipe[1]); // we have a copy already, so close it
+	return 0;
+}
+
+
+int execFile(char * cmd, char * args[], int* rpipe, int* wpipe)
+{
+	pid_t cpid;
+	
+	cpid = fork();
+	
+	if(cpid < 0)
+	{
+		printf("\n Fork Failed \n");
+		return -1;
+	}
+	else if(cpid == 0) 					// Child
+	{
+		if(rpipe) // there's a pipe from the previous process
+            set_read(rpipe);
+        // else you may want to redirect input from somewhere else for the start
+        
+		if(wpipe) // there's a pipe to the next process
+            set_write(wpipe);
+        // else you may want to redirect out to somewhere else for the end
+		
+		//ExecV file
+		execv(cmd,args);
+	}
+	else								// Parent
+	{
+		//wait();							// Wait for fpid, command to complete
+		return 0;
+	}
+	
+	return 0;
+	
+}
+
+//Find the path of the command in PATH env variable and execute
+//
+int findAndExecute(char *cmd, char*args[], int* rpipe, int* wpipe)
+{
+	char *PATH = "/bin:/usr/bin";
+	char paths[10][256];
+	
+	char cfp[256]; 							// full path of cmd
+	int pn=0,i,r1;
+	
+	//Parse PATH and get individual paths
+	strcpy(paths[0],"/usr/bin");
+	strcpy(paths[1],"/bin");
+	strcpy(paths[2],"../../bin");
+	pn=3;
+	
+	for(i=0;i<pn;i++)
+	{
+		strcat(cfp,paths[i]);
+		strcat(cfp,"/");
+		strcat(cfp,cmd);
+		if(checkIfExecutable(cfp) == 0)
+		{
+			//printf("File %s is executable \n",cfp);
+			break;
+		}
+		//else
+		//	printf("%s cannot be found or is not an executable\n",cfp);
+			
+		strcpy(cfp,"");
+	}
+	if(strlen(cfp)==0)
+	{
+		printf("%s: command not found \n",cmd);
+		return -1;
+	}
+	printf("Executing command: %s \n",cfp);
+	
+	execFile(cfp,args,rpipe,wpipe);
+	
+	return 0;
+}
+
+
+
